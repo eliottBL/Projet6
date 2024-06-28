1df9a5fa9e7551fc6d6c822d876b35e20259f4e7
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-06-28 16:34:35 +0200

    feat:app return json message

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res) => {
+    res.json({ message: 'votre requête à bient été prise en compte' });
+});
 
 module.exports = app;
