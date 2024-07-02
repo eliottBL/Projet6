20fcd934d8449deee797e95a7e48f0b9d55519ea
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-07-02 08:45:56 +0200

    feat(app.js): utilisation models thing, POST save new thing, GET find things, routes non AJ

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ mongoose.connect('mongodb+srv://eliott:eliott@clustermvg.yghy6ec.mongodb.net/?re
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+app.use(express.json());
+
+const Thing = require('./models/thing');
+
 
 app.use((req, res) => {
     res.json({ message: 'votre requête à bient été prise en compte' });
@@ -20,29 +24,21 @@ app.use((req, res, next) => {
     next();
 });
 
-/*
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
-*/
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
+});
 
 
 module.exports = app;
