a0b10e59410dc05079cf62087ea94a62de6821bb
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-07-12 12:22:32 +0200

    fix: création controllers>stuff et simplification fichier routes>stuff

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 mongoose.connect('mongodb+srv://eliott:eliott@clustermvg.yghy6ec.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMVG',
     {
         useNewUrlParser: true,
@@ -9,14 +10,7 @@ mongoose.connect('mongodb+srv://eliott:eliott@clustermvg.yghy6ec.mongodb.net/?re
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-app.use(express.json());
 
-const Thing = require('./models/thing');
-
-
-app.use((req, res) => {
-    res.json({ message: 'votre requête à bient été prise en compte' });
-});
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -24,35 +18,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/books', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
+
+app.use(express.json());
+
+app.use((req, res) => {
+    res.json({ message: 'votre requête à bient été prise en compte' });
 });
 
-app.use('/api/books', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/books/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-});
+app.use('api/stuff', stuffRoutes);
 
 module.exports = app;
